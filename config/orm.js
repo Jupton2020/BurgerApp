8670ea7327e7b9bcb9676cd5ff784efba705c954
jack upt <jhonbahama@gmail.com>
2020-01-17 23:52:38 -0800

    added page functionality

--- config/orm.js
+++ config/orm.js
@@ -12,7 +12,7 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
     var arr = [];
 
-    for (var ky in ob) {
+    for (var key in ob) {
         var value = ob[key];
         if (Object.hasOwnProperty.call(ob, key)) {
             if(typeof value === "string" && value.indexOf(" ") >= 0){
@@ -35,7 +35,7 @@ var orm = {
             cb(result);
         });
     },
-    insertOne: function(table, cols, vals, cb){
+    insertOne: function (table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
 
         queryString += " (";
@@ -54,6 +54,22 @@ var orm = {
 
             cb(result);  
         });
+    },
+    updateOne: function(table, objColVals, condition, cb){
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString +=  objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function( err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        });
+
     },
 };
 module.exports = orm;
