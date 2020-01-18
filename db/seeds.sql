61e648e687a8d20549219fe996dd007b18c71586
jack upt <jhonbahama@gmail.com>
2020-01-17 21:57:28 -0800

    added packages

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO burgers (burger_name, devoured)
 VALUES ("Big Mac", true);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Big 'N Tasty'", true);
+VALUES ("Big 'N Tasty", true);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Quarter Pounder w/Cheese", true);
